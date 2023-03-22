463aeefb57b2311d1388260014b7290a0a876880
Tarun <ts371714@gmail.com>
2023-03-22 15:15:01 +0530

    first commit

---
+++ file.py
@@ -0,0 +1,16 @@
+#fy = open("entry.csv")
+#filestring=fy.read()
+#print(filestring)
+
+#filestring=fy.readline()
+#print(filestring)
+
+#for fileline in fy:
+ #   linearray=fileline.split(" , ")
+    #print(linearray)
+    #if(len(linearray)>0):
+  #  if linearray[0]=="tarun":
+   #     print(linearray[0])
+        #break
+
+filewrite=
