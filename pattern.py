32dd0479b4dfe435fbd87c7d2fa12a6f4960a4b8
Tarun <ts371714@gmail.com>
2023-03-22 15:23:14 +0530

    first commit

---
+++ pattern.py
@@ -0,0 +1,11 @@
+
+for i in range(5):
+    for x in range (i):
+        print("*",end=" ")
+    print(" ")
+
+for a in range(5,0,-1):
+    for b in range (a):
+        print("*",end=" ")
+    print(" ")
+
