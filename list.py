c6ffce3f7377915c0d40a0fe15d8a36e8878ffdc
Tarun <ts371714@gmail.com>
2023-03-22 15:18:07 +0530

    first commit

---
+++ list.py
@@ -0,0 +1,8 @@
+names = ["rayith", "Tarun", "Khushbu", "Ankan"]
+feedbacks = [3, 4, 5, 3.5]
+for name in range(4):
+   print(names[name], "has given", feedbacks[name] , "as a feedback")
+
+names.append("monu")
+feedbacks.append("4")
+print(names)
