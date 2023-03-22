29ce691de799bda26a93917e9018d925af2313b6
Tarun <ts371714@gmail.com>
2023-03-22 15:24:28 +0530

    first commit

---
+++ start.py
@@ -0,0 +1,14 @@
+
+from email.errors import MultipartInvariantViolationDefect
+
+
+name= "tarun"
+print(name)
+name = "laxmi"
+print(name)
+a=input("enter your name")
+print(a)
+b=input("enter your sencon number")
+print(b)
+multiplicationVariable= int(a)*int(b)
+print(multiplicationVariable)
