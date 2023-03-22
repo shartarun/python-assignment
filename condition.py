aa382af2cbae4dadd380b055667dada180a08767
Tarun <ts371714@gmail.com>
2023-03-22 15:09:10 +0530

    first commit

---
+++ condition.py
@@ -0,0 +1,13 @@
+input=20
+number=0
+print(number)
+number=number+1
+print(number)
+if(number<=input):
+    print(number)
+    number=number+1
+else:
+    print("end of program")
+#for loop program
+for index in range(1,input,2):
+    print(index)
