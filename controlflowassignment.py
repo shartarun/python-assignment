e1a65dbb80fca6602148cc9af816d3d139243022
Tarun <ts371714@gmail.com>
2023-03-22 15:11:09 +0530

    first commit

---
+++ controlflowassignment.py
@@ -0,0 +1,10 @@
+introduction =  499.00
+Python = 855.00
+Data_Science = 645.00
+gst= 12
+deliveryCharegs=250
+totalAmount= introduction + Python + Data_Science
+TotalBill= totalAmount * gst /100
+TotalBills= deliveryCharegs + TotalBill + totalAmount
+print(TotalBills)
+
